b34f31fb46fc20405bb376177b051d330884181e
Deon Butler <deonbutler44@gmail.com>
2022-05-11 22:25:46 -1000

    completed milestone 3.

--- public_html/documentation/script.js
+++ public_html/documentation/script.js
@@ -55,3 +55,34 @@ if (currentTheme) {
 function fadeIn() {
     document.body.className = 'fadeIn'
 }
+
+document.addEventListener("click", e => {
+    let handle
+    if (e.target.matches(".handle")) {
+        handle = e.target
+    } else {
+        handle = e.target.closest(".handle")
+    }
+    if (handle !== null) {
+        onHandleClick(handle)
+    }
+})
+
+function onHandleClick(handle) {
+    const slider = handle.closest(".portfolio-container").querySelector(".slider")
+    const sliderIndex = parseInt(getComputedStyle(slider).getPropertyValue("--slider-index"))
+    if (handle.classList.contains("left-handle")) {
+        if (sliderIndex < 1) {
+            slider.style.setProperty("--slider-index", slider.children.length - 1)
+        } else {
+            slider.style.setProperty("--slider-index", sliderIndex - 1)
+        }
+    }
+    if (handle.classList.contains("right-handle")) {
+        if (sliderIndex + 1 >= slider.children.length) {
+            slider.style.setProperty("--slider-index", 0)
+        } else {
+            slider.style.setProperty("--slider-index", sliderIndex + 1)
+        }
+    }
+}
